c3a98417ec6c70444087c6d2853681e43c7cda0d
pou <paul.ou@nfl.com>
2020-08-10 13:56:21 -0700

    check

--- src/com/po/quiz/design/Codec.java
+++ src/com/po/quiz/design/Codec.java
@@ -2,64 +2,36 @@ package com.po.quiz.design;
 
 import com.po.leetcode.TreeNode;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
+
+/***
+ * which tree traversal ensure the sequence???
+ *
+ */
 
 public class Codec {
-    private String NULL = "null";
-    private String COMMA = ",";
+    String DELIMITER = ",";
+    String NULL = "#";
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
         StringBuilder sb = new StringBuilder();
-        sb.append("[");
-        if(root == null){
-            sb.append(NULL);
-            sb.append("]");
-            return sb.toString();
-        }
+        serializeHelper(root, sb);
+        return null;
+    }
 
-        //dfs
-        Stack<TreeNode> stack = new Stack<>();
-        stack.push(root);
-        while (!stack.isEmpty()){
-            TreeNode node = stack.pop();
-            if(sb.charAt(sb.length() - 1) != '['){
-                sb.append(COMMA);
-            }
-            if(node == null){
-                sb.append(NULL);
-            } else {
-                sb.append(node.val);
-                stack.push(node.right);
-                stack.push(node.left);
-            }
+    private void serializeHelper(TreeNode node, StringBuilder sb){
+        if(node == null) return;
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(node);
+        while(node != null && !stack.isEmpty()){
+            node = stack.pop();
+            sb.append(node.val);
+            sb.append(DELIMITER);
         }
-        sb.append(']');
-        return sb.toString();
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        if(data == null || data.length() == 0) return null;
-
-        data = data.substring(1, data.length() - 1); //remove '[' and ']'
-        if(data.length() == 0) return null; //for case : []
-
-        String[] strs = data.split(",");
-        return deserializeHelper(new LinkedList<>(Arrays.asList(strs)));
-    }
-
-    private TreeNode deserializeHelper(List<String> list){
-        String val = list.get(0);
-        list.remove(0);
-        if(val.equals(NULL)){
-            return null;
-        }
-        TreeNode node = new TreeNode(Integer.valueOf(val));
-        node.left = deserializeHelper(list);
-        node.right = deserializeHelper(list);
-        return node;
+        return null;
     }
 }
---
+++ src/com/po/quiz/dp/wordbreak/Q472.java
@@ -0,0 +1,9 @@
+package com.po.quiz.dp.wordbreak;
+
+import java.util.List;
+
+public class Q472 {
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+        return null;
+    }
+}
